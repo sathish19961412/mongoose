d3ca06b814560c0d12a6a5cc16efa626211caf79
sathish19961412 <sathishfreelanc5@gmail.com>
2023-10-03 18:14:26 +0530

    create to async to the await the function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const User =require('./models/userModels');
-const Product=require('./models/productModels')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/sathishcode').then(()=>{
     console.log('connected')
@@ -9,19 +9,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/sathishcode').then(()=>{
 })
 
 const user = new User({
-     name:'sathish',
-     age:26
+     name:'guru',
+     age:24
 });
+// user.save().then(()=>{
+//     console.log('User Save')
+// })
 
-const product=new Product({
-    productname:'T-Shirt',
-    productprice:1000
-});
-
-user.save().then(()=>{
-    console.log('User Save')
-})
-
-product.save().then(()=>{
-    console.log('Product Save')
-})
+// const product=new Product({
+//     productname:'T-Shirt',
+//     productprice:1000
+// });
+async function run(){
+    const newuser=await user.save()
+        console.log(newuser)
+}
+run();
